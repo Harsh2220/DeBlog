2284b4dc1d944183155d4bd6988629ca8f9199f2
Harsh <harshgajjar145@gmail.com>
2022-12-08 18:33:05 +0530

    fea: Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 import { Heading } from '@chakra-ui/react'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <Heading>
-      Hello
-    </Heading>
+    <Hero />
   )
 }
 
