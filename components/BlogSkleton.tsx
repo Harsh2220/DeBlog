23bc27239b102b55395234e5f2dfd42f425538c0
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-12-10 13:21:32 +0530

    fix: loader

--- components/BlogSkleton.tsx
+++ components/BlogSkleton.tsx
@@ -12,12 +12,12 @@ import React from "react";
 function BlogSkleton() {
   return (
     <Box
-          padding="6"
-          my={1}
-      w={["96", "3xl"]}
-      boxShadow="lg"
+      padding="6"
+      my={1}
+      minW={["80", "3xl"]}
+      maxW={["80", "3xl"]}
       bg={useColorModeValue("white", "gray.800")}
-      >
+    >
       <SkeletonCircle size="10" />
       <SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="2" />
     </Box>
