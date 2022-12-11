35c8f787dfbcf8c0b36e8d5c90c0495048ef6b29
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-12-11 12:14:01 +0530

    Basic MArkdown Supported

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
-import type { NextPage } from 'next'
-import { useState, useEffect } from 'react';
-import Sidebar from '../components/Sidebar';
+import type { NextPage } from "next";
+import { useState, useEffect } from "react";
+import Sidebar from "../components/Sidebar";
 import { ethers } from "ethers";
 import { CONTRACT_ADDRESS, ABI } from "../utils/utils";
-import useStore from '../store/store';
+import useStore from "../store/Store";
 const Home: NextPage = () => {
   const state = useStore();
   const setBlogs = state.setBlogs;
-  const [detailBlogs, setDetailBlogs] = useState('');
-
+  const [detailBlogs, setDetailBlogs] = useState("");
   useEffect(() => {
-    getAllBlogs()
-  }, [detailBlogs])
+    getAllBlogs();
+  }, [detailBlogs]);
   const getAllBlogs = async () => {
     try {
       const { ethereum } = window;
@@ -19,11 +18,7 @@ const Home: NextPage = () => {
         if (ethereum) {
           const provider = new ethers.providers.Web3Provider(ethereum, "any");
           const signer = provider.getSigner();
-          const DeBlog = new ethers.Contract(
-            CONTRACT_ADDRESS,
-            ABI,
-            signer
-          );
+          const DeBlog = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
           const AllBlogs = await DeBlog.getAllblogs();
           console.log(AllBlogs);
 
@@ -34,10 +29,8 @@ const Home: NextPage = () => {
     } catch (error) {
       console.log(error);
     }
-  }
-  return (
-    <Sidebar blogs={detailBlogs} />
-  )
-}
+  };
+  return <Sidebar blogs={detailBlogs} />;
+};
 
-export default Home
+export default Home;
--- pages/write.tsx
+++ pages/write.tsx
@@ -1,29 +1,59 @@
-import { Container,Input, Stack, Box , Flex, Divider, Textarea} from '@chakra-ui/react'
-import React from 'react'
-import ImageUpload from '../components/ImageUpload'
+import {
+  Container,
+  Input,
+  Stack,
+  Box,
+  Flex,
+  Divider,
+  Textarea,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import ImageUpload from "../components/ImageUpload";
 
-type Props = {}
+type Props = {};
 
 const write = (props: Props) => {
+  const [markdown, setMarkdown] = useState<string>("");
   return (
-    <Container maxW={'container.md'}>
-        <Flex justifyContent={'space-between'} alignItems={'center'} paddingBlock={8}>
-        <Stack w={'sm'}>
-            <Box w={'full'} marginBlock={4}>
-                <Input htmlSize={4} width='full' placeholder='Title' size={'lg'}/>
-            </Box>
-            <Box w={'full'} marginBlock={4}>
-                <Input htmlSize={4} width='full' placeholder='SubTitle' size={'lg'}/>
-            </Box>
+    <Container maxW={"container.md"}>
+      <Flex
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        paddingBlock={8}
+      >
+        <Stack w={"sm"}>
+          <Box w={"full"} marginBlock={4}>
+            <Input htmlSize={4} width="full" placeholder="Title" size={"lg"} />
+          </Box>
+          <Box w={"full"} marginBlock={4}>
+            <Input
+              htmlSize={4}
+              width="full"
+              placeholder="SubTitle"
+              size={"lg"}
+            />
+          </Box>
         </Stack>
         <ImageUpload />
-        </Flex>
-       <Divider height={'full'} borderBottomWidth={'3px'}/>
-       <Box mt={8}>
-        <Textarea border={'none'} _focusVisible={{borderColor: 'transparent'}} resize={'none'} placeholder='Write something' fontSize={'1.3rem'}/>
-       </Box>
+      </Flex>
+      <Divider height={"full"} borderBottomWidth={"3px"} />
+      <Box mt={8}>
+        <Textarea
+          rows={5}
+          border={"none"}
+          _focusVisible={{ borderColor: "transparent" }}
+          resize={"none"}
+          onChange={(e) => {
+            setMarkdown(e.target.value);
+          }}
+          placeholder="Write something"
+          fontSize={"1.3rem"}
+        />
+      </Box>
+      <ReactMarkdown children={markdown} />
     </Container>
-  )
-}
+  );
+};
 
-export default write
+export default write;
