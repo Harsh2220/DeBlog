9addc0610e9c5ca87263af85959307da4dfafaea
Umang Patel <umangpatel3105@gmail.com>
2022-12-16 15:36:58 +0530

    gradient

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -12,17 +12,19 @@ import {
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
-import { AiOutlineRead } from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineRead } from "react-icons/ai";
 import useStore from "../../store/Store";
 import readingTime from "../../utils/getReadingTime";
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown from 'react-markdown'
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
 
-const MDPreview = dynamic(() => import("@uiw/react-markdown-preview"), {
-  ssr: false,
-});
+
+const MDPreview = dynamic(
+  () => import("@uiw/react-markdown-preview"),
+  { ssr: false }
+);
 
 type blogPageProps = {
   title: string;
@@ -40,12 +42,12 @@ const BlogPage = ({ title, subTitle, banner, content }: blogPageProps) => {
   const blogs = state.blogs;
   const { id } = router.query;
   const currentBlog = blogs[id];
-  const [first, setfirst] = useState("");
-
+  const [first, setfirst] = useState('');
+  
   // console.log(currentBlog);
 
-  return path == "/write" ? (
-    <HStack
+  return (
+    path == '/write'?(<HStack
       mt={"2%"}
       // border={"2px solid"}
       justifyContent={"center"}
@@ -86,11 +88,11 @@ const BlogPage = ({ title, subTitle, banner, content }: blogPageProps) => {
           </Stack>
           <VStack>
             <Text fontSize={"4xl"} fontWeight={700}>
-              {title ? title : "Add Title"}
+              {title?title:'Add Title'}
             </Text>
             <Box w={["auto", "3xl"]} height={"sm"}>
               <Image
-                src={banner ? URL.createObjectURL(banner) : "/blogImage.webp"}
+                src={banner?URL.createObjectURL(banner):'/blogImage.webp'}
                 borderRadius={10}
                 p={"2"}
                 height={"100%"}
@@ -99,115 +101,35 @@ const BlogPage = ({ title, subTitle, banner, content }: blogPageProps) => {
             </Box>
             {/* <MDEditor value={content} onChange={setfirst} preview="preview"/> */}
             <Box w={["auto", "3xl"]}>
-              <MDPreview
-                source={content}
-                fullscreen={true}
-                style={{
-                  backgroundColor: "#ffffff",
-                  color: "#000000",
-                  padding: "10px",
-                }}
-              />
+              <MDPreview source={content} fullscreen={true} style={{ backgroundColor: '#ffffff', color: '#000000', padding:'10px' }}/>
             </Box>
             {/* <Text textAlign={"justify"}>{content?content:'Add Blog Content'}</Text> */}
           </VStack>
         </Box>
       </HStack>
-    </HStack>
-  ) : (
+    </HStack>):<HStack
+    mt={"2%"}
+    // border={"2px solid"}
+    justifyContent={"center"}
+    position={"relative"}
+    alignItems={"flex-start"}
+    paddingTop={"10"}
+    gap={10}
+  >
     <HStack
-      mt={"2%"}
-      // border={"2px solid"}
-      justifyContent={"center"}
-      position={"relative"}
+      justifyContent={"space-between"}
       alignItems={"flex-start"}
-      paddingTop={"10"}
-      gap={10}
+      position={"relative"}
     >
-      <HStack
-        justifyContent={"space-between"}
-        alignItems={"flex-start"}
-        position={"relative"}
-      >
-        <Box
-          maxW={["96", "3xl"]}
-          // border={"2px solid"}
-          bg={useColorModeValue("white", "gray.800")}
-          p={4}
-          borderColor={"gray"}
-        >
-          <Stack>
-            <HStack border={"1px solid linear(to-r, green.200, pink.500)"}>
-              <Avatar
-                size={"sm"}
-                src="https://i.pravatar.cc/300"
-                outline={"2px solid pink"}
-                outlineColor={useColorModeValue("purple", "pink")}
-                outlineOffset={"2px"}
-              />
-              <Text fontSize={"lg"} fontWeight={700} lineHeight={"0.8"}>
-                {/* {currentBlog.authorName} */}
-                gwrbdfb
-              </Text>
-            </HStack>
-            <Flex opacity={0.6}>
-              <Text>cathalmacdonnacha.com</Text>
-              <Text ml={4}>Dec 6, 2022</Text>
-            </Flex>
-          </Stack>
-          <Flex mt={2} flexDirection={["column", "column", "column", "row"]}>
-            <Stack pr={2} py={[1]}>
-              <Flex alignItems={"center"}>
-                <AiOutlineRead />{" "}
-                <Text lineHeight={0.8} ml={4} fontWeight={"500"}>
-                  {/* {readingTime(currentBlog.blogContent)} min read */}
-                  fbdfb
-                </Text>
-              </Flex>
-            </Stack>
-          </Flex>
-          <VStack>
-            <Text fontSize={"4xl"} fontWeight={700}>
-              {/* {currentBlog.blogTitle} */}
-              dfbdbsz
-            </Text>
-            <Box w={["auto", "3xl"]} height={"sm"}>
-              <Image
-                // src={currentBlog.coverImage}
-                src="/walletconnect.png"
-                borderRadius={10}
-                p={"2"}
-                height={"100%"}
-                width={"100%"}
-              />
-            </Box>
-            <Text textAlign={"justify"}>
-              {/* {currentBlog.blogContent} */}
-              zfdbsdgb
-            </Text>
-          </VStack>
-        </Box>
-      </HStack>
       <Box
-        borderRadius={"sm"}
-        bgGradient='linear(to-l, #7928CA, #FF0080)'
-        p={"0.15%"}
-        w={"20%"}
+        maxW={["96", "3xl"]}
+        // border={"2px solid"}
+        bg={useColorModeValue("white", "gray.800")}
+        p={4}
+        borderColor={"gray"}
       >
-        <Box
-          // height={"30vh"}
-          borderRadius={'sm'}
-          position={"sticky"}
-          w={"100%"}
-          backgroundColor={useColorModeValue("white","gray.800")}
-          top={"10"}
-          display={"flex"}
-          flexDirection={"column"}
-          p={"4"}
-          gap={"4"}
-          justifyContent={"center"}
-        >
-          <HStack>
+        <Stack>
+          <HStack border={"1px solid linear(to-r, green.200, pink.500)"}>
             <Avatar
               size={"sm"}
               src="https://i.pravatar.cc/300"
@@ -216,13 +138,70 @@ const BlogPage = ({ title, subTitle, banner, content }: blogPageProps) => {
               outlineOffset={"2px"}
             />
             <Text fontSize={"lg"} fontWeight={700} lineHeight={"0.8"}>
-              {/* {currentBlog.authorName} */}
+              {currentBlog.authorName}
             </Text>
           </HStack>
-          <Button bg={"blue.300"}>FOLLOW</Button>
-        </Box>
+          <Flex opacity={0.6}>
+            <Text>cathalmacdonnacha.com</Text>
+            <Text ml={4}>Dec 6, 2022</Text>
+          </Flex>
+        </Stack>
+        <Flex mt={2} flexDirection={["column", "column", "column", "row"]}>
+          <Stack pr={2} py={[1]}>
+            <Flex alignItems={"center"}>
+              <AiOutlineRead />{" "}
+              <Text lineHeight={0.8} ml={4} fontWeight={"500"}>
+                {readingTime(currentBlog.blogContent)} min read
+              </Text>
+            </Flex>
+          </Stack>
+        </Flex>
+        <VStack>
+          <Text fontSize={"4xl"} fontWeight={700}>
+            {currentBlog.blogTitle}
+          </Text>
+          <Box w={["auto", "3xl"]} height={"sm"}>
+            <Image
+              src={currentBlog.coverImage}
+              borderRadius={10}
+              p={"2"}
+              height={"100%"}
+              width={"100%"}
+            />
+          </Box>
+          <Box w={["auto", "3xl"]}>
+              <MDPreview source={currentBlog.blogContent} fullscreen={true} style={{ backgroundColor: "transparent", color: useColorModeValue("black", "white"), padding:'10px' }}/>
+            </Box>
+        </VStack>
       </Box>
     </HStack>
+    <Box
+      // height={"30vh"}
+      border={"2px solid"}
+      w={"20%"}
+      position={"sticky"}
+      top={"10"}
+      display={"flex"}
+      flexDirection={"column"}
+      p={"4"}
+      gap={"4"}
+      justifyContent={"center"}
+    >
+      <HStack>
+        <Avatar
+          size={"sm"}
+          src="https://i.pravatar.cc/300"
+          outline={"2px solid pink"}
+          outlineColor={useColorModeValue("purple", "pink")}
+          outlineOffset={"2px"}
+        />
+        <Text fontSize={"lg"} fontWeight={700} lineHeight={"0.8"}>
+          {currentBlog.authorName}
+        </Text>
+      </HStack>
+      <Button bg={"blue.300"}>Send Tip <AiOutlineHeart/>  </Button>
+    </Box>
+  </HStack>
   );
 };
 export default BlogPage;
